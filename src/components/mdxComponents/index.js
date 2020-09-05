eab22c227b36ea91c48dc05fa13012542cf9aae0
Owen Young <theowenyoung@gmail.com>
2020-09-06 03:53:18 +0800

    feat: change theme to github

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -4,11 +4,6 @@ import styled from '@emotion/styled';
 import CodeBlock from './codeBlock';
 // import AnchorTag from './anchor';
 
-const StyledPre = styled('pre')`
-  padding: 16px;
-  background: ${props => props.theme.colors.preFormattedText};
-`;
-
 export default {
   h1: props => {
     return <h1 className="heading1" {...props} />;
@@ -19,16 +14,6 @@ export default {
   h5: props => <h5 className="heading5" {...props} />,
   h6: props => <h6 className="heading6" {...props} />,
   p: props => <p className="paragraph" {...props} />,
-  pre: props => (
-    <StyledPre>
-      <pre {...props} />
-    </StyledPre>
-  ),
+
   code: CodeBlock,
-  // a: AnchorTag,
-  // TODO add `img`
-  // TODO add `blockquote`
-  // TODO add `ul`
-  // TODO add `li`
-  // TODO add `table`
 };
